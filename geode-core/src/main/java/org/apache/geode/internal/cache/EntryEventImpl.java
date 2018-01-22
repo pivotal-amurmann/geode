e3acadd7b6ef960f58f06c5a3cb3df337c7314c8
Barry Oglesby <boglesby@users.noreply.github.com>
2018-01-22 14:44:05 -0800

    GEODE-4306: Added concurrent checks enabled test to event time
    (cherry picked from commit ea7721613a4d3ccde904a3ad820ecd2b4f9f5527)

--- geode-core/src/main/java/org/apache/geode/internal/cache/EntryEventImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/EntryEventImpl.java
@@ -2548,7 +2548,7 @@ public class EntryEventImpl
    */
   public long getEventTime(long suggestedTime) {
     long result = suggestedTime;
-    if (this.versionTag != null) {
+    if (this.versionTag != null && getRegion().getConcurrencyChecksEnabled()) {
       if (suggestedTime != 0) {
         this.versionTag.setVersionTimeStamp(suggestedTime);
       } else {
